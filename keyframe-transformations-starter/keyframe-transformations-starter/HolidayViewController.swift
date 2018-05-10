15d83d5bddb83d451f27dd6980b41ab8187cc707
Barney <barneym29@gmail.com>
2018-05-10 11:34:58 +0300

    Scale Transformation

--- keyframe-transformations-starter/keyframe-transformations-starter/HolidayViewController.swift
+++ keyframe-transformations-starter/keyframe-transformations-starter/HolidayViewController.swift
@@ -5,14 +5,38 @@ class HolidayViewController: UIViewController {
 
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var darkView: CustomUIView!
-    @IBOutlet weak var shareButton: CustomButton!
+    @IBOutlet weak var shareButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    @IBAction func toggleShareMenu(_ sender: CustomButton) {
+    @IBAction func toggleShareMenu(_ sender: UIButton) {
+        
+        if darkView.transform == .identity {
+            UIView.animate(withDuration: 3.0,
+                           animations: {
+                            
+                            self.darkView.backgroundColor = .black
+                            self.darkView.transform = CGAffineTransform(scaleX: 15, y: 15)
+            },
+                           completion: {_ in
+                            
+            })
+        } else {
+            
+            UIView.animate(withDuration: 3.0,
+                           animations: {
+                            
+                            self.darkView.transform = .identity
+                            self.darkView.backgroundColor = .clear
+            },
+                           completion: {_ in
+                            
+            })
+            
+        }
     }
     
 }
