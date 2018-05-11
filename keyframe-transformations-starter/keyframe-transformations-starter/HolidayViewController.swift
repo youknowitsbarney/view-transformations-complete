74236239c00252e032ee86f3d8acf4648a42c6ba
Barney <barneym29@gmail.com>
2018-05-11 10:15:53 +0300

    Rotation and Translation Transformation

--- keyframe-transformations-starter/keyframe-transformations-starter/HolidayViewController.swift
+++ keyframe-transformations-starter/keyframe-transformations-starter/HolidayViewController.swift
@@ -18,25 +18,38 @@ class HolidayViewController: UIViewController {
             UIView.animate(withDuration: 3.0,
                            animations: {
                             
+                            self.shareButton.setImage(UIImage(named: "plus"), for: .normal)
                             self.darkView.backgroundColor = .black
                             self.darkView.transform = CGAffineTransform(scaleX: 15, y: 15)
+                            self.shareButton.transform = CGAffineTransform(rotationAngle: self.toRadians(45))
+                                .concatenating(CGAffineTransform(translationX: 320, y: 0))
+
             },
                            completion: {_ in
                             
             })
+            
         } else {
             
             UIView.animate(withDuration: 3.0,
                            animations: {
                             
+                            self.shareButton.transform = .identity
                             self.darkView.transform = .identity
+                            self.shareButton.setImage(UIImage(named: "share"), for: .normal)
                             self.darkView.backgroundColor = .clear
+                            
             },
                            completion: {_ in
                             
             })
             
         }
+    }
+    
+    private func toRadians(_ degrees: CGFloat) -> CGFloat {
+        
+        return (degrees * .pi / 180)
     }
     
 }
